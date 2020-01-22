ad6d819382bc6ecedbcfee806d4f0325e9c91160
Steve Bottelbergs <steve.bottelbergs@continuum.be>
2020-01-22 20:41:01 +0100

    Remove persistence

--- src/main/java/be/syntra/java/advanced/persistence/PersistenceConfig.java
+++
@@ -1,9 +0,0 @@
-package be.syntra.java.advanced.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories
-public class PersistenceConfig {
-}
